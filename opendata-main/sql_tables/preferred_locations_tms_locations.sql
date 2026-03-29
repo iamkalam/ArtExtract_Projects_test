625e157800dd8443d87a9ef9caab0ff1f13841a2
abdul kalam <vishalbaig6@gmail.com>
2026-03-29 20:08:43 +0500

    checking for clear code pushing

--- opendata-main/sql_tables/preferred_locations_tms_locations.sql
+++
@@ -1,6 +0,0 @@
-                             Table "preferred_locations_tms_locations"
-        Column        |         Type          | Collation | Nullable | Default 
-----------------------+-----------------------+-----------+----------+---------
- preferredlocationkey | character varying(32) |           |          | 
- tmslocationid        | integer               |           |          | 
-
